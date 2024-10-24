30a8e9bff884b716555a9ab48c61a7494b3ba631
Thomas Pericoi <thomaspericoi@gmail.com>
2024-10-24 15:40:32 +0200

    feat(single-avs_job): add introduction

--- single-avs_job.php
+++ single-avs_job.php
@@ -1,13 +1,33 @@
 <?php get_header(); ?>
 
 <!-- Hero -->
-<section id="hero-<?php echo uniqid(); ?>" class="hero hero-simple">
+<section id="hero-<?php echo uniqid(); ?>" class="hero hero-simple" style="--color-theme:<?php echo get_field("block_color"); ?>">
     <div class="container">
         <div class="breadcrumbs">
             <?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
         </div>
         <div class="h4-size parent-title"><?php echo __("Nos métiers"); ?></div>
         <h1><?php echo get_the_title(); ?></h1>
+    </div>
+
+    <div class="thumbnail" style="background-image: url('<?php echo get_field("thumbnail"); ?>');"></div>
+
+    <div class="introduction">
+        <div class="container container-lg">
+            <div class="cols-wrapper">
+                <div class="col formatted">
+                    <?php if (get_field("introduction_title")) : ?>
+                        <h2 class="h3-size"><?php echo get_field("introduction_title"); ?></h2>
+                    <?php endif; ?>
+                    <?php if (get_field("introduction_content")) : ?>
+                        <?php echo get_field("introduction_content"); ?>
+                    <?php endif; ?>
+                </div>
+                <div class="col">
+                    <p># TO DO : Formulaire</p>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
