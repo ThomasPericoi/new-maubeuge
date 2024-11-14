02695469b203bf6858e9273bfd2b6e74e547f054
Thomas Pericoi <thomaspericoi@gmail.com>
2024-11-14 11:29:34 +0100

    feat(join-us-template): add template

--- templates/join-us-template.php
+++ templates/join-us-template.php
@@ -9,13 +9,34 @@ get_header(); ?>
             <?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
         </div>
         <h1><?php echo get_the_title(); ?></h1>
+        <?php if (get_field("join_us_hero_text")) : ?>
+            <p><?php echo get_field("join_us_hero_text"); ?></p>
+        <?php endif; ?>
     </div>
 </section>
 
 <!-- Dispatch -->
-<section id="dispatch-<?php echo uniqid(); ?>" class="dispatch">
-    <p># TO DO</p>
-</section>
+<?php if (have_rows('join_us_cards_cards')) : ?>
+    <section id="dispatch-<?php echo uniqid(); ?>" class="dispatch">
+        <div class="cols-wrapper">
+            <?php while (have_rows('join_us_cards_cards')) : the_row();
+                $background = get_sub_field('background');
+                $title = get_sub_field('title');
+                $text = get_sub_field('text');
+                $link = get_sub_field('link'); ?>
+
+                <a href="<?php echo $link; ?>" class="col card" style="background-image: url('<?php echo $background; ?>');">
+                    <div class="container">
+                        <h2><?php echo $title; ?></h2>
+                        <p><?php echo $text; ?></p>
+                        <span class="btn btn-secondary"><?php echo __("En savoir plus", "new-maubeuge"); ?></span>
+                    </div>
+                </a>
+
+            <?php endwhile; ?>
+        </div>
+    </section>
+<?php endif; ?>
 
 <?php get_template_part('template-parts/insights-list', '', array(
     'title' => get_field("join_us_insights_title"),
