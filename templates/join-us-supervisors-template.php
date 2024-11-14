37d613b83da000539396d6e6f3ce933fa9899197
Thomas Pericoi <thomaspericoi@gmail.com>
2024-11-14 18:30:49 +0100

    feat(join-us-supervisors-template): add template

--- templates/join-us-supervisors-template.php
+++ templates/join-us-supervisors-template.php
@@ -9,21 +9,73 @@ get_header(); ?>
             <?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
         </div>
         <h1><?php echo get_field("title_alt") ?: get_the_title(); ?></h1>
-        <div class="jobs-list">
-            <p># TO DO : Liste de postes</p>
-        </div>
+        <?php if (have_rows('join_us_supervisors_offers')) : ?>
+            <div class="jobs-list">
+                <?php while (have_rows('join_us_supervisors_offers')) : the_row();
+                    $title = get_sub_field('title');
+                    $city = get_sub_field('city');
+                    $type = get_sub_field('type');
+                    $document = get_sub_field('document');
+                    $link = get_sub_field('link'); ?>
+                    <div class="job">
+                        <div class="col">
+                            <?php if ($title) : ?>
+                                <h2 class="h5-size"><?php echo $title; ?></h2>
+                            <?php endif; ?>
+                            <div class="informations">
+                                <?php if ($city) : ?>
+                                    <div class="city">
+                                        <?php get_template_part('assets/icons/location-circled.svg'); ?><span><?php echo $city; ?></span>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if ($type) : ?>
+                                    <div class="type">
+                                        <?php get_template_part('assets/icons/contract-circled.svg'); ?><span><?php echo $type; ?></span>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="btn-wrapper">
+                                <?php if ($document) : ?>
+                                    <a href="<?php echo $document; ?>" target="_blank" class="btn btn-icon-pdf"><?php echo __("Visualiser l'offre", "new-maubeuge"); ?></a>
+                                <?php endif; ?>
+                                <?php if ($link) : ?>
+                                    <a href="<?php echo $link; ?>" target="_blank" class="btn btn-primary btn-icon-linkedin"><?php echo __("Visualiser sur LinkedIn", "new-maubeuge"); ?></a>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        <?php endif; ?>
     </div>
 </section>
 
-<!-- Our Jobs -->
-<section id="jobs-description-<?php echo uniqid(); ?>" class="jobs-description">
-    <div class="container">
-        <?php if (get_field("join_us_supervisors_jobs_description_title")) : ?>
-            <h2><?php echo get_field("join_us_supervisors_jobs_description_title"); ?></h2>
-        <?php endif; ?>
-        <p># TO DO</p>
-    </div>
-</section>
+<?php if (have_rows('join_us_supervisors_jobs_description_rows')) : ?>
+    <!-- Our Jobs -->
+    <section id="jobs-description-<?php echo uniqid(); ?>" class="jobs-description">
+        <div class="container">
+            <?php if (get_field("join_us_supervisors_jobs_description_title")) : ?>
+                <h2><?php echo get_field("join_us_supervisors_jobs_description_title"); ?></h2>
+            <?php endif; ?>
+            <div class="jobs-description-grid">
+                <?php while (have_rows('join_us_supervisors_jobs_description_rows')) : the_row();
+                    $title = get_sub_field('title');
+                    $content = get_sub_field('content'); ?>
+                    <div class="job-description formatted">
+                        <?php if ($title) : ?>
+                            <h3 class="h5-size"><?php echo $title; ?></h3>
+                        <?php endif; ?>
+                        <?php if ($content) : ?>
+                            <?php echo $content; ?>
+                        <?php endif; ?>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>
 
 <!-- Rows -->
 <?php if (have_rows('join_us_supervisors_content_rows')) : ?>
@@ -31,7 +83,7 @@ get_header(); ?>
         $title = get_sub_field('title');
         $content = get_sub_field('content');
         $image = get_sub_field('image');
-        $direction = (get_row_index() % 2)? 'reverse': 'normal' ?>
+        $direction = (get_row_index() % 2) ? 'reverse' : 'normal' ?>
 
         <?php get_template_part('template-parts/row-2-columns', 'regular', array(
             'title' => $title,
